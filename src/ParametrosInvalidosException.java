23e9b5fb6d3619c2d7cb59e659fb24f5189dcf46
braatz90 <156862622+braatz90@users.noreply.github.com>
2024-06-24 16:54:48 -0300

    Add files via upload

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administra
+ */
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+        
+    }
+    
+}
